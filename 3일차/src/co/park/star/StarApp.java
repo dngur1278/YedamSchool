7edb3c8ed556f867f1fb6f494ba73db2a3b3a2e0
woo <woo@222.104.195.207>
2021-05-31 00:17:07 +0900

    수정

--- 3일차/src/co/park/star/StarApp.java
+++ 3일차/src/co/park/star/StarApp.java
@@ -34,21 +34,26 @@ public class StarApp {
 	//이등변 삼각형
 	void drawEq(int cnt) {
 		for(int i=1; i<cnt; i++) {
-			System.out.print(pad(' ', cnt-i+2));
+			System.out.print(pad(' ', 50+(cnt-i)));
 			System.out.println(pad('*', i*2-1));
 		}
 	}
 	
 	void drawEq(int a, int cnt) {
 		for(int i=a; i<cnt; i++) {
-			System.out.print(pad(' ', cnt-i));
+			if (cnt<7) {
+				System.out.print(pad(' ', 48+(cnt-i)));
+			}
+			else if (cnt<9) {
+				System.out.print(pad(' ', 46+(cnt-i)));
+			}
 			System.out.println(pad('*', i*2-1));
 		}
 	}
 	
 	void drawRact(int cnt) {
 		for(int i=1; i<=cnt; i++) {
-			System.out.print(pad(' ', cnt+2));
+			System.out.print(pad(' ', 46+(cnt+2)));
 			System.out.println(pad('*', cnt-1));
 		}
 	}
